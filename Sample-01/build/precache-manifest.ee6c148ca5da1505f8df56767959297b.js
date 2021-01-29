749a52e1ccb218a8e8f16ab45f52eb221aa989a1
fridman.resume <fridman.resume@gmail.com>
2021-01-29 08:38:02 -0500

    misc1

---
+++ Sample-01/build/precache-manifest.ee6c148ca5da1505f8df56767959297b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712f3d8dc193f7a4010cc06c4198732e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "990dd565f31991c65edd",
+    "url": "/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "9351d9e03129d07b62a5",
+    "url": "/static/css/main.1aa3f63c.chunk.css"
+  },
+  {
+    "revision": "990dd565f31991c65edd",
+    "url": "/static/js/2.e6941c31.chunk.js"
+  },
+  {
+    "revision": "2f631157007bf1ef36424afacfd94f3b",
+    "url": "/static/js/2.e6941c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9351d9e03129d07b62a5",
+    "url": "/static/js/main.163b9dd6.chunk.js"
+  },
+  {
+    "revision": "0231f95ac667b73c9187",
+    "url": "/static/js/runtime-main.91e21c86.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
